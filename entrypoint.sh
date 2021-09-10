df7e1d128d4ce864f910e13cb35024f0b0c6b6ee
magician8520 <34188062+magician8520@users.noreply.github.com>
2021-09-11 02:52:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,13 @@ DIR_TMP="$(mktemp -d)"
 cat << EOF > ${DIR_TMP}/heroku.json
 {
     "inbounds": [{
-        "port": ${PORT},
-        "protocol": "vmess",
+        "tag": "tg-in",
+        "port": 443,
+        "protocol": "mtproto",
         "settings": {
-            "clients": [{
-                "id": "${ID}",
-                "alterId": ${AID}
-            }]
-        },
-        "streamSettings": {
-            "network": "ws",
-            "wsSettings": {
-                "path": "${WSPATH}"
-            }
+        "users": [{"secret": "b0cbcef5a486d9636472ac27f8e11a9d"}]
         }
-    }],
+}],
     "outbounds": [{
         "protocol": "freedom"
     }]
