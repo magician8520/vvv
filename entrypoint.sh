f40faae36338b6a5a4f99b898d43f7bcccab6f60
magician8520 <34188062+magician8520@users.noreply.github.com>
2021-09-11 03:37:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,19 +22,6 @@ cat << EOF > ${DIR_TMP}/heroku.json
         ]
       }
     },
-    {
-      "tag": "tg-in",
-      "port": 934,
-      "protocol": "mtproto",
-      "settings": {
-        "users": [
-          {
-            "secret": "b0cbcef5a486d9636472ac27f8e11a9d"
-          }
-        ]
-      }
-    }
-  ],
   "outbounds": [
     {
       "protocol": "freedom",
